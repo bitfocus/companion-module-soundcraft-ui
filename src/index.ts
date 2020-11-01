ffd0282bfd93b05956b945e4c35abdd1fce186ce
Ferdinand Malcher <ferdinand@malcher.media>
2020-11-01 15:22:12 +0100

    feat: reflect connection status

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import InstanceSkel = require('../../../instance_skel');
 import { CompanionConfigField, CompanionSystem } from '../../../instance_skel_types';
-import { SoundcraftUI } from 'soundcraft-ui-connection';
+import { SoundcraftUI, ConnectionStatus, ConnectionErrorEvent } from 'soundcraft-ui-connection';
 import { GetActionsList } from './actions';
 import { GetConfigFields, UiConfig } from './config';
 import { GetFeedbacksList } from './feedback';
@@ -22,14 +22,54 @@ class SoundcraftUiInstance extends InstanceSkel<UiConfig> {
    * is OK to start doing things.
    */
   public init(): void {
+    this.status(this.STATUS_UNKNOWN);
+    this.createConnection();
+  }
+
+  /**
+   * Create new mixer connection object,
+   * start connection and set things up
+   */
+  private createConnection() {
     if (this.config.host) {
       this.conn = new SoundcraftUI(this.config.host);
       this.conn.connect();
-      console.log('CONNECT!!');
+
+      this.subscribeConnectionStatus();
       this.updateCompanionBits();
     }
   }
 
+  /**
+   * Consume the status of the connection and map it to companion status flags
+   */
+  private subscribeConnectionStatus() {
+    if (!this.conn) {
+      return;
+    }
+
+    this.conn.status$.subscribe(status => {
+      switch (status.type) {
+        case ConnectionStatus.Opening:
+          this.status(this.STATUS_WARNING, 'Connecting');
+          break;
+        case ConnectionStatus.Error:
+          this.status(this.STATUS_ERROR, (status as ConnectionErrorEvent).payload.message);
+          break;
+        case ConnectionStatus.Open:
+          this.status(this.STATUS_OK);
+          break;
+        case ConnectionStatus.Close:
+          this.status(this.STATUS_ERROR, 'Disconnected');
+          break;
+      }
+    });
+  }
+
+  /**
+   * set up all companion specific things for this module
+   * such as actions, feedback and presets.
+   */
   private updateCompanionBits(): void {
     this.setActions(GetActionsList(this.conn));
     this.setFeedbackDefinitions(GetFeedbacksList(this, this.state, this.conn));
@@ -43,16 +83,14 @@ class SoundcraftUiInstance extends InstanceSkel<UiConfig> {
     // if host has changed, reconnect
     if (this.config.host !== config.host) {
       this.conn.disconnect();
-      this.conn = new SoundcraftUI(config.host);
-      this.conn.connect();
-      this.updateCompanionBits();
+      this.createConnection();
     }
 
     this.config = config;
   }
 
   /**
-   * Creates the configuration fields for web config.
+   * Create the configuration fields for web config.
    */
   // eslint-disable-next-line @typescript-eslint/camelcase
   public config_fields(): CompanionConfigField[] {
